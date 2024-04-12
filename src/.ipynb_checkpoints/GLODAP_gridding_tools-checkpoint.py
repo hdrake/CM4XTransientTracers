447cc5d20387e875a9ce6227e02589a13c2ad812
Anthony Meza <ameza98@outlook.com>
2024-04-12 16:28:18 -0400

    testing

---
+++ src/.ipynb_checkpoints/GLODAP_gridding_tools-checkpoint.py
@@ -0,0 +1,121 @@
+import pyinterp
+import pyinterp.backends.xarray
+from scipy import interpolate
+import xarray as xr
+import dask
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy  # Map projections libary
+import cartopy.crs as ccrs  # Projections list
+from scipy.interpolate import CubicSpline
+import copy
+import pandas as pd
+
+def extract_GLODAP_vars(ds, varn): 
+    lat, lon = ds["G2latitude"].to_xarray(), ds["G2longitude"].to_xarray()
+    years = ds["G2year"].to_xarray()
+    depths = ds["G2depth"].to_xarray()
+    
+    cast_num = ds["G2cast"].to_xarray().astype("int").astype("str")
+    cruise_num = ds["G2cruise"].to_xarray().astype("int").astype("str")
+    station_num = ds["G2station"].to_xarray().astype("int").astype("str")
+
+    cast_codes = ds["G2expocode"].to_xarray().astype("str")
+    
+    tmp = np.char.add(cast_codes.values, "_")
+    tmp = np.char.add(tmp, cruise_num)
+    tmp = np.char.add(tmp, "_")
+    tmp = np.char.add(tmp, station_num)
+    tmp = np.char.add(tmp, "_")
+    tmp = np.char.add(tmp, cast_num)
+    
+    all_casts = copy.deepcopy(cast_codes)
+    all_casts.values = tmp
+
+    return lat, lon, years, depths, all_casts
+
+def GLODAP_2_regular_grid(ds, regular_z, regular_lat, regular_lon, varns, year = 1990.):
+
+    nvars = len(varns)
+    nreglon = len(regular_lon)
+    nreglat = len(regular_lat)
+    nregz = len(regular_z)
+    
+    vars_dicts = dict()
+    casts_dict = dict()
+    
+    for (v, varn) in enumerate(varns): 
+        vars_gridded_nearest = np.nan * np.zeros((nreglon, nreglat, nregz))
+        lat, lon, years, depths, all_casts = extract_GLODAP_vars(ds, varn)
+        var_df = ds[varn].to_xarray()
+        try: 
+            var_df_flag = ds[varn + "f"].to_xarray()
+            which_year_and_notnan = (years == year) * (~np.isnan(var_df)) * ( var_df_flag == 2)
+        except: 
+            which_year_and_notnan = (years == year) * (~np.isnan(var_df))
+
+        depths_ = depths[which_year_and_notnan]
+        lat_  = lat[which_year_and_notnan]
+        lon_ = lon[which_year_and_notnan]
+        var_ = var_df[which_year_and_notnan]
+        all_casts_ = all_casts[which_year_and_notnan]
+        all_casts_ = np.array(all_casts_.values.tolist())
+        unique_casts_ = np.unique(all_casts_)
+        
+        interp_casts = np.zeros((len(regular_z), len(unique_casts_)))
+        LATS = np.zeros(len(unique_casts_))
+        LONS = np.zeros(len(unique_casts_))
+        cast_list = []
+        for j in range(len(unique_casts_)):
+            which_cast = (all_casts_ == unique_casts_[j])
+            cast_list += [unique_casts_[j]]
+            cast_var = var_[which_cast]; cast_depths = depths_[which_cast]
+            
+            unique_depths = np.unique(cast_depths)
+            LATS[j] = lat_[which_cast][0]
+            LONS[j] = lon_[which_cast][0]
+            time_averaged_cast = np.zeros(len(unique_depths))
+            if len(unique_depths) < len(cast_depths):
+                for (i, d) in enumerate(unique_depths):
+                    time_averaged_cast[i] = np.nanmean(cast_var[cast_depths == d]) 
+            else:
+                time_averaged_cast[:] = 1 * cast_var[:]  
+
+            f = interpolate.interp1d(unique_depths, time_averaged_cast, fill_value = np.nan, 
+                                     bounds_error = False, kind = "linear")
+            interp_casts[:, j] = f(regular_z)
+            
+        interp_casts_ds = xr.Dataset(
+                        coords={
+                        "points":(["points"], np.arange(len(unique_casts_))),
+                        "z":(["z"], regular_z)
+                        })
+        interp_casts_ds[varn] = (["z", "points"], interp_casts)
+        interp_casts_ds["lat"] = (["points"], LATS)
+        interp_casts_ds["lon"] = (["points"], LONS)
+        interp_casts_ds["cast_id"] = (["points"], cast_list)
+        interp_casts_ds["time"] = year
+
+        for k in range(len(regular_z)):
+            binning = pyinterp.Binning2D(
+                pyinterp.Axis(regular_lon, is_circle=True),
+                pyinterp.Axis(regular_lat))
+            binning.clear()
+            binning.push(LONS, LATS, 1 * interp_casts[k, :][:], True)        
+            vars_gridded_nearest[:, :, k] = 1 * binning.variable('mean')            
+            
+        regular_ds = xr.Dataset(
+                        coords={
+                        "lon":(["lon"], regular_lon),
+                        "lat":(["lat"], regular_lat),
+                        "z":(["z"], regular_z), 
+                        })
+        regular_ds[varn] = (["lon", "lat", "z"], vars_gridded_nearest)
+        regular_ds["time"] = year
+
+        vars_dicts[varn] = regular_ds
+        casts_dict[varn] = interp_casts_ds
+
+    
+    
+    return (vars_dicts, casts_dict)
---
+++ src/.ipynb_checkpoints/src-checkpoint.py
@@ -0,0 +1,261 @@
+import xarray as xr 
+from scipy.interpolate import interp1d
+from scipy.interpolate import CubicSpline
+import numpy as np
+import geopy
+from geopy import distance
+import xesmf as xe
+import sectionate
+import re
+import gsw
+
+
+def approximate_z(ds):
+    tmp = ds.thkcello.cumsum(dim = "zl")
+    #average between 0 and cell bottom
+    tmp1 = tmp.isel(zl = 0) / 2 
+    #get top of cell
+    tmp2 = tmp.isel(zl = slice(0, -1)) 
+    #get bottom of cell
+    tmp3 = tmp.isel(zl = slice(1, None)) 
+    #make sure cell interfaces are on same coordinate
+    tmp2.coords['zl'] = tmp3.coords['zl']
+    #take average
+    tmp4 = (tmp2 + tmp3) / 2
+
+    ds["z"] = xr.concat([-tmp1, -tmp4], dim = "zl")    
+    
+    return ds
+    
+    
+def get_thetao(g_ds): #get potential temperature for a GLODAPP section 
+    g_ds["z"] = -g_ds.depth
+    g_ds = g_ds.rename({"salinity":"so"})
+
+    g_ds['p'] = xr.apply_ufunc(
+        gsw.p_from_z, g_ds.z, g_ds.lat, 0, 0, dask="parallelized"
+    )
+    g_ds['sa'] = xr.apply_ufunc(
+        gsw.SA_from_SP,
+        g_ds.so,
+        g_ds.p,
+        g_ds.lon,
+        g_ds.lat,
+        dask="parallelized",
+    )
+
+    g_ds['thetao'] = xr.apply_ufunc(
+        gsw.pt0_from_t,
+        g_ds.sa,
+        g_ds.theta,
+        g_ds.p,
+        dask="parallelized",
+    )
+    return g_ds
+
+def get_sigma2(ds, keep_vars = False): 
+    ds['p'] = xr.apply_ufunc(
+        gsw.p_from_z, ds.z, ds.lat, 0, 0, dask="parallelized"
+    )
+
+
+    ds['sa'] = xr.apply_ufunc(
+        gsw.SA_from_SP,
+        ds.so,
+        ds.p,
+        ds.lon,
+        ds.lat,
+        dask="parallelized",
+    )
+    ds['ct'] = xr.apply_ufunc(
+        gsw.CT_from_t,
+        ds.sa,
+        ds.thetao,
+        ds.p,
+        dask="parallelized"
+    )
+
+    ds['sigma2'] = xr.apply_ufunc(
+        gsw.sigma2,
+        ds.sa,
+        ds.ct,
+        dask="parallelized"
+    )
+    if keep_vars: 
+        return ds
+    else: 
+        return ds.drop_vars(["p", "sa", "ct"])
+    
+    
+
+
+def interpolate_values(ds, original_grid, objective_grid, kind = "linear"):
+    unique_points, unique_inds = np.unique(original_grid.values, return_index = True)
+    
+    if len(unique_inds) > 1:
+        ds_subset = ds.values[unique_inds]
+        where_nonan = ~np.isnan(unique_points + ds_subset)
+
+        if len(ds_subset[where_nonan]) > 1:
+            interp = interp1d(unique_points[where_nonan], ds_subset[where_nonan], 
+                              bounds_error = False, kind = kind, 
+                              fill_value=np.nan, assume_sorted = False)
+            return interp(objective_grid)
+        else:
+            print("hi")
+            return np.nan * objective_grid
+    else: 
+        return np.nan * objective_grid
+    
+    
+def interpolate_section(ds, objective_grid, 
+                        interp_coord = "z", 
+                        iterate_coord = "distance",
+                        kind = "linear"):
+    
+    interp_ds = xr.Dataset(coords={interp_coord:objective_grid, 
+                                   iterate_coord:ds[iterate_coord].values})
+        
+    for key,value in ds.drop(interp_coord).items():
+        tmp_array = np.zeros((len(objective_grid), len(ds[iterate_coord])))
+        for i in range(len(ds[iterate_coord])):
+            tmp = interpolate_values(ds[key].isel({iterate_coord:i}), 
+                                     ds[interp_coord].isel({iterate_coord:i}), 
+                                     objective_grid, kind = kind)
+            tmp_array[:, i] = 1 * tmp
+
+        interp_ds = interp_ds.assign({key:([interp_coord,iterate_coord], 1 * tmp_array)})
+
+    return interp_ds
+
+def match_GLODAPP_WOCE(gdset, wdset, time = '2005-2014'):
+    years = re.findall(r'\d+', wdset.attrs['all_years_used'])
+    ecs = wdset.attrs['expocode'].split(',')
+                                         
+    ecs_i = list(set(ecs) & set(list(gdset['G2expocode'].unique())))
+    print('Available cruise data : '+' '.join(ecs_i))
+                                         
+    # Subset GLODAP data for available cruise
+    cond = gdset['G2expocode']==''
+    for ec in ecs:
+        cond += (gdset['G2expocode']==ec)
+    dfo_all = gdset[cond]
+                                         
+    # Find cruises that fall within model averaging period
+    year_bnds = [int(a) for a in (time.split("-"))]
+    leeway = 1
+    cond = dfo_all['G2year']==''
+    for year in dfo_all['G2year'].unique():
+        if year_bnds[0]-leeway <= year <= year_bnds[1]+leeway:
+            cond += (dfo_all['G2year']==year)
+    dfo = dfo_all[cond]
+    if len(dfo)==0:
+        yearstr = [str(e) for e in dfo_all['G2year'].unique()]
+        raise Exception('No suitable GLODAP data for selected WOCE line and year\n'+
+                       'Avaliable data: '+' '.join(ecs_i)+'\n'+
+                       'For years: '+' '.join(yearstr))
+    return dfo
+                                         
+def calc_dz(depth):
+    d1 = depth.copy()
+    zeros = xr.zeros_like(d1.isel(n=0)).expand_dims({'ni':[0]})
+    ids = 0.5*(d1+d1.shift(n=-1)).rename({'n':'ni'}).assign_coords({'ni':d1['n'].values+1})
+    idepth = xr.concat([zeros,ids],dim='ni')
+    dz = idepth.diff('ni')
+    dz.loc[{'ni':len(dz['ni'])}]=dz.isel(ni=-2)
+    dz = dz.rename({'ni':'n'}).assign_coords({'n':d1['n']})
+    return dz
+
+def calc_dz_1d(depth):
+    ids = 0.5*(depth[1:]+depth[:-1])
+    idepth = np.append(np.array([0]),ids)
+    dz = np.diff(idepth)
+    dz = np.append(dz,dz[-1])
+    return dz
+    
+def calc_dx(distance):
+    d1 = distance.values
+    mid = 0.5*(d1[1:]+d1[:-1])
+    mid = np.append(-mid[0],mid)
+    dx = np.diff(mid)
+    dx = np.append(dx,dx[-1])
+    return xr.DataArray(dx,dims=distance.dims,coords=distance.coords)    
+
+def extract_GLODAPP_cruises(dfo):                         
+    glodap_variables = ['cfc11','cfc12','sf6','theta','salinity']
+    sections = {}
+    cruises = dfo['G2cruise'].unique()
+    for cruise in cruises:
+        dfonow = dfo[dfo['G2cruise']==cruise]
+        stations = dfonow['G2station'].unique()
+
+        ns = len(stations)
+
+        # Find max no. measurements at each station
+        # And distance along section
+        maxn = 0
+        distance = np.zeros(shape=(ns,))
+        avg_time = np.nanmean(dfonow["G2year"].values)
+
+        for i,station in enumerate(stations):
+            dfostation = dfonow[dfonow['G2station']==station]
+            if i==0:
+                lonlast = dfostation['G2longitude'].mean()
+                latlast = dfostation['G2latitude'].mean()
+            else:
+                lon = dfostation['G2longitude'].mean()
+                lat = dfostation['G2latitude'].mean()
+                distance[i]=geopy.distance.distance((lat,lon),(latlast,lonlast)).km
+            if len(dfostation)>maxn:
+                maxn = len(dfostation)
+
+        nb = maxn
+
+        # Populate numpy arrays
+        lons = np.full(shape=(ns,),fill_value=np.nan)
+        lats = np.full(shape=(ns,),fill_value=np.nan)
+        depths = np.full(shape=(ns,nb),fill_value=np.nan)
+        dzs = np.full(shape=(ns,nb),fill_value=np.nan)
+        variables = {}
+        for gv in glodap_variables:
+            variables[gv]=np.full(shape=(ns,nb),fill_value=np.nan)
+        for i,station in enumerate(stations):
+            dfostation = dfonow[dfonow['G2station']==station]
+            n = len(dfostation)
+
+            lon = dfostation['G2longitude'].mean()
+            lat = dfostation['G2latitude'].mean()
+            depth = dfostation['G2depth']
+            lons[i,]=lon
+            lats[i,]=lat
+            depths[i,:n]=depth
+            dzs[i,:n] = calc_dz_1d(depth.values)
+
+            for variable in variables.keys():
+                tmp = dfostation['G2'+variable]
+                # Get the variable at unique depths by creating a dictionary
+                dt = dict(zip(depth,tmp))
+                # Recreate variable at all depths
+                tmpnow = [dt[d] for d in depth]
+                variables[variable][i,:n] = tmpnow
+
+        # Assign to dataset
+        section = xr.Dataset({
+            'lon':xr.DataArray(lons,dims=('distance'),coords={'distance':np.cumsum(distance)}),
+            'lat':xr.DataArray(lats,dims=('distance'),coords={'distance':np.cumsum(distance)}),
+            'depth':xr.DataArray(depths,dims=('distance','n'),coords={'distance':np.cumsum(distance)}),
+            'dz':xr.DataArray(dzs,dims=('distance','n'),coords={'distance':np.cumsum(distance)})}
+        )
+        section["time"] = avg_time
+        for variable,array in variables.items():
+            section[variable] = xr.DataArray(array,dims=('distance','n'),coords={'distance':np.cumsum(distance)})
+
+        # Calculate appropriate "grid variables"
+        section['dx'] = calc_dx(section['distance'])
+
+        sections[cruise]=section
+                                         
+    
+    return sections
+                                         
+                                         
